b41a505e80fe83992c01da42a7a8690880f40e12
Maciek Gorczyca <maciek.gorczyca@poczta.fm>
2019-04-27 11:05:08 +0200

    deklaracje

---
+++ MCTS/MCTS.cs
@@ -0,0 +1,33 @@
+﻿namespace MCTS
+{
+    public class Mcts
+    {
+        // konstruktor
+
+        private Node _rootNode;
+
+        private Node _currNode;
+
+//        private int 
+
+        public void MakeMove(int column)
+        {
+            // todo
+        }
+
+        public int GenerateMove()
+        {
+            return 0; //todo
+        }
+
+        public Board GetBoard()
+        {
+            return _currNode.CurrBoard;
+        }
+
+        private void Search()
+        {
+
+        }
+    }
+}
